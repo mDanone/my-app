e09bcdd176fb85512842f522aac259989684192a
mDanone <daniyalbek.daniyalov@gmail.com>
2022-08-19 01:55:02 +0300

    clear main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import CalendarBlock from './CalendarBlock';
+import DateBlock from './DateBlock';
+
+
 function App() {
+  let [columnNumber, setColumnNumber] = useState(3)
+
+  const getHours = () => {
+    const currentHours = new Date().getHours()
+    return currentHours
+  }
+  let items=[[1, 2, 3], [4, 5, 6]];
+
+  setTimeout(() => {const hours = getHours(); console.log(hours);}, 3000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DateBlock/>
+      {items.map((item,index)=>{
+          item.forEach(element => {
+            return <CalendarBlock/>
+          });
+          return <CalendarBlock/>
+      })}
     </div>
   );
 }
